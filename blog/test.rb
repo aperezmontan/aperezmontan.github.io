0a0ae60277d372c5cd2b65a8ea5cfc12b171ef12
Ari Perez <aris.a.perez@gmail.com>
2015-03-08 23:08:22 -0400

    Adding to my blog for week 5

---
+++ blog/test.rb
@@ -0,0 +1,20 @@
+class Car
+     def initialize
+          @engine = "V6"
+     end
+
+     def engine
+          @engine
+     end
+
+     def wheels(sizeOfWheels)
+          if sizeOfWheels > 20
+           @engine = "V8"
+          end
+     end
+end
+
+truck = Car.new
+p truck.engine
+p truck.wheels(24)
+p truck.engine
